39247a0666061c5a9c03c3997f17aebe87d038ef
Tiago Aguiar <tyago_tka48@hotmail.com>
2020-01-16 22:53:47 -0300

    Criado o layout e aplicado toda a estilizaçao

--- src/App.js
+++ src/App.js
@@ -1,13 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header'
 
+import './style.css'
+import './app.css'
+import './sidebar.css'
+import './main.css'
+
 function App() {
+
   return (
-    <div className="App">
-      <Header title="Dashboard" />
-      <Header title="Título" />
-      <Header title="Tela" />
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input type="text" name="github_username" id="github_username" required />
+
+          </div>
+          
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input type="text" name="techs" id="techs" required />
+          </div>
+          
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input type="text" name="latitude" id="latitude" />
+            </div>
+            
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input type="text" name="longitude" id="longitude" />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://cdn1.iconfinder.com/data/icons/avatar-3/512/Manager-512.png" alt="Diego Fernandes" />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento</p>
+            <a href="#">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://cdn1.iconfinder.com/data/icons/avatar-3/512/Manager-512.png" alt="Diego Fernandes" />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento</p>
+            <a href="#">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://cdn1.iconfinder.com/data/icons/avatar-3/512/Manager-512.png" alt="Diego Fernandes" />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento</p>
+            <a href="#">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://cdn1.iconfinder.com/data/icons/avatar-3/512/Manager-512.png" alt="Diego Fernandes" />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento</p>
+            <a href="#">Acessar perfil no github</a>
+          </li>
+          
+        </ul>
+      </main>
     </div>
   );
 }
